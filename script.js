d3cf97000485fb2305c93fef2801645d634e18a8
Sayan Malgope <140529971+malgopesayan@users.noreply.github.com>
2023-08-17 20:18:09 +0530

    script.js

--- script.js
+++ script.js
@@ -22,3 +22,36 @@ function addTask() {
     taskInput.value = "";
   }
 }
+
+function editTask(taskItem, editButton) {
+  const taskText = taskItem.querySelector(".task-text");
+  const originalText = taskText.textContent;
+  const editInput = document.createElement("input");
+  editInput.value = originalText;
+  taskItem.replaceChild(editInput, taskText);
+
+  editButton.textContent = "Save";
+  editButton.classList.remove("edit-btn");
+  editButton.classList.add("save-btn");
+
+  editButton.removeEventListener("click", handleEdit);
+  editButton.addEventListener("click", function() {
+    saveTask(taskItem, editInput, editButton);
+  });
+}
+
+function saveTask(taskItem, editInput, saveButton) {
+  const editedText = editInput.value.trim();
+  const taskText = document.createElement("span");
+  taskText.textContent = editedText;
+
+  taskItem.replaceChild(taskText, editInput);
+
+  saveButton.textContent = "Edit";
+  saveButton.classList.remove("save-btn");
+  saveButton.classList.add("edit-btn");
+
+  saveButton.removeEventListener("click", saveTask);
+  saveButton.addEventListener("click", handleEdit);
+}
+
